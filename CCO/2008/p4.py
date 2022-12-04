c3aa86e351929c4a75daa67c6bd77a5e04bfad24
kellen-sun <ksun5@ocdsb.ca>
2022-12-04 11:18:40 -0500

    fixed bug

--- CCO/2008/p4.py
+++ CCO/2008/p4.py
@@ -1,50 +1,48 @@
-#basically change the grid into a graph and then count how many connected components there are
+
 import sys
 from collections import deque
 input = sys.stdin.readline
 n,m = map(int, input().split())
-grid = [input() for i in range(n)]
+grid = [input()[:-1] for i in range(n)]
 
-def init_queued():
+def init_queued(a):
     global n
     global m
     queued = {}
     for i in range(n):
         for j in range(m):
-            queued[(i,j)]=False
+            queued[(i,j)]=a
     return queued
-
-in_component = init_queued()
-components = 0
+graph = [[] for i in range(n*m)]
 for i in range(n):
     for j in range(m):
-        if not in_component[(i,j)]:
+        if grid[i][j]=="N":
+            graph[i*n+j].append(i*n-n+j)
+            graph[i*n-n+j].append(i*n+j)
+        elif grid[i][j]=="S":
+            graph[i*n+j].append(i*n+j+n)
+            graph[i*n+j+n].append(i*n+j)
+        elif grid[i][j]=="W":
+            graph[i*n+j].append(i*n+j-1)
+            graph[i*n+j-1].append(i*n+j)
+        elif grid[i][j]=="E":
+            graph[i*n+j].append(i*n+j+1)
+            graph[i*n+j+1].append(i*n+j)
+in_component = [False for i in range(n*m)]
+components = 0
+#print(graph)
+for i in range(n):
+    for j in range(m):
+        if not in_component[i*n+j]:
             components+=1
-            queue = deque([(i,j)])
-            #queued = init_queued()
+            queue = deque([i*n+j])
+            in_component[i*n+j]=True
             while queue:
                 current = queue.popleft()
-                cur_value = grid[current[0]][current[1]]
-                if cur_value=="S":
-                    if not in_component[(current[0]+1, current[1])]:
-                        #queued[(current[0]-1, current[1])] = True
-                        queue.append((current[0]+1, current[1]))
-                        in_component[(current[0]+1, current[1])]=True
-                if cur_value=="N":
-                    if not in_component[(current[0]-1, current[1])]:
-                        #queued[(current[0]-1, current[1])] = True
-                        queue.append((current[0]-1, current[1]))
-                        in_component[(current[0]-1, current[1])]=True
-                if cur_value=="W":
-                    if not in_component[(current[0], current[1]-1)]:
-                        #queued[(current[0]-1, current[1])] = True
-                        queue.append((current[0], current[1]-1))
-                        in_component[(current[0], current[1]-1)]=True
-                if cur_value=="E":
-                    if not in_component[(current[0], current[1]+1)]:
-                        #queued[(current[0]-1, current[1])] = True
-                        queue.append((current[0], current[1]+1))
-                        in_component[(current[0], current[1]+1)]=True
-            #issue is that right now might not be starting on first node kinda of the component, should prolly make it so that edges are bidirectional so bfs search backwards and forwards.
-print(components)
+                #print(current)
+                for kk in graph[current]:
+                    if not in_component[kk]:
+                        queue.append(kk)
+                        in_component[kk]=True
 
+print(components)
