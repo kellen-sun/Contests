8d0d898126c37484a93369ca48cc745a44cddd6e
kellen-sun <ksun5@ocdsb.ca>
2022-12-06 14:00:55 -0500

    work on 2016 s3

--- CCC Senior/CCC Senior 2016/s3.py
+++ CCC Senior/CCC Senior 2016/s3.py
@@ -1,34 +1,58 @@
-from itertools import permutations
 n,m=map(int, input().split())
 pho = list(map(int, input().split()))
-graph={}
-for i in range(n):
-    graph[i]=[]
+graph=[[] for i in range(n)]
 for i in range(n-1):
-    a,b=list(map(int, input().split()))
+    a,b = map(int, input().split())
     graph[a].append(b)
     graph[b].append(a)
-#print(graph)
+queue = []
 
-def traverse(start,end,graph):
-    visited=[]
-    queue=[[start,0]]
-    while queue:
-        node = queue.pop()
-        dist=node[1]
-        if node[0]==end:
-            return node[1]
-        for i in graph[node[0]]:
-            if i not in visited:
-                queue.append([i, dist+1])
-                visited.append(i)
-                
-a=float("inf")
-for i in list(permutations(pho, m)):
-    s=0
-    for j in range(m-1):
-        s+=traverse(i[j],i[j+1],graph)
-    #print("PATH: ", i, "LENGTH:",s)
-    a=min(a,s)
-    
-print(a)
+for i in range(n):
+    if len(graph[i]) == 1:
+        queue.append(i)
+visited = [False for i in range(n)]
+while queue:
+    current = queue.pop(0)
+    if current not in pho and not visited[current] and len(graph[current])==1:
+        parent = graph[current][0]
+        queue.append(parent)
+        graph[current]=[]
+        graph[parent].remove(current)
+        visited[current]=True
+for i in range(n):
+    if graph[i]!=[]:
+        start = i
+        break
+queue = [start]
+dist = [0 for i in range(n)]
+queued = [False for i in range(n)]
+queued[start]=True
+while queue:
+    current = queue.pop(0)
+    for i in graph[current]:
+        if not queued[i]:
+            dist[i]=dist[current]+1
+            queue.append(i)
+            queued[i]=True
+start = -1
+m = -1
+for i in range(len(dist)):
+    if dist[i]>m:
+        m=dist[i]
+        start = i
+
+queue = [start]
+dist = [0 for i in range(n)]
+queued = [False for i in range(n)]
+queued[start]=True
+while queue:
+    current = queue.pop(0)
+    for i in graph[current]:
+        if not queued[i]:
+            dist[i]=dist[current]+1
+            queue.append(i)
+            queued[i]=True
+mmm=0
+for i in graph:
+    if i!=[]: mmm+=1
+print(2*mmm-2-max(dist))
