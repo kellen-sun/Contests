22ac67678e3bb22013e0814c195a9b83b5cc3997
kellen-sun <ksun5@ocdsb.ca>
2022-12-07 20:21:16 -0500

    .

--- CCC Senior/CCC Senior 2015/s4.py
+++ CCC Senior/CCC Senior 2015/s4.py
@@ -1,6 +1,5 @@
 import sys
 import heapq
-from collections import deque
 input = sys.stdin.readline
 
 k,n,m = map(int, input().split())
@@ -23,7 +22,6 @@ while dist:
     u = heapq.heappop(dist)
     d = u[0]
     node = u[1]
-
     for v in graph[node]:
         for add_dmg in range(k-v[2]):
             alt = dd[node][add_dmg]+v[1]
@@ -32,7 +30,5 @@ while dist:
                 dd[v[0]][total_dmg]=alt
                 heapq.heappush(dist, (alt, v[0]))
 a = min(dd[b])
-if a==float("inf"):
-    print(-1)
-else:
-    print(a)
+if a==float("inf"): print(-1)
+else: print(a)
