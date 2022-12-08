9a2077154e797356dfb76e38f95c77cd1c7c77d2
kellen-sun <ksun5@ocdsb.ca>
2022-12-07 19:08:38 -0500

    delete visited (not needed) it's slightly faster

--- CCC Senior/CCC Senior 2015/s4.py
+++ CCC Senior/CCC Senior 2015/s4.py
@@ -14,24 +14,23 @@ for i in edges:
     graph[i[0]-1].append((i[1]-1, i[2], i[3]))
     graph[i[1]-1].append((i[0]-1, i[2], i[3]))
 
-dist = [(0,a,0)]
+dist = [(0,a)]#,0)]
 heapq.heapify(dist)
 dd = [[float('inf') for i in range(k)] for j in range(n)]
 dd[a][0] = 0
-visited = [[False for i in range(k)] for j in range(n)]
+
 while dist:
     u = heapq.heappop(dist)
     d = u[0]
     node = u[1]
-    damage = u[2]
-    visited[node][damage] = True
+
     for v in graph[node]:
-        #print(v)
         for add_dmg in range(k-v[2]):
             alt = dd[node][add_dmg]+v[1]
-            if dd[v[0]][add_dmg+v[2]]>alt and not visited[v[0]][add_dmg+v[2]]:
-                dd[v[0]][add_dmg+v[2]]=alt
-                heapq.heappush(dist, (alt, v[0], add_dmg+v[2]))
+            total_dmg = add_dmg+v[2]
+            if dd[v[0]][total_dmg]>alt:
+                dd[v[0]][total_dmg]=alt
+                heapq.heappush(dist, (alt, v[0]))
 a = min(dd[b])
 if a==float("inf"):
     print(-1)
