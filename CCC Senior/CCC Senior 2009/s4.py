5f7ff9bf417ad6bf2100af3e856bc379d54262c0
kellen-sun <ksun5@ocdsb.ca>
2022-12-16 21:43:09 -0500

    attempted optimizations

--- CCC Senior/CCC Senior 2009/s4.py
+++ CCC Senior/CCC Senior 2009/s4.py
@@ -1,44 +1,42 @@
 #then convert dijkstra's from the node u want a pencil from and find the cheapest choice.
 #add the pencil costs to all the destination city costs (removing the ones that don't sell)
 import sys
+import heapq
 input = sys.stdin.readline
+
 n = int(input())
 t = int(input())
-roads = [list(map(int, input().split())) for i in range(t)]
+#adjacency matrix uses too much memory
+graph = [[] for i in range(n)]
+for i in range(t):
+    a,b,c=map(int, input().split())
+    graph[a-1].append((b-1,c))
+    graph[b-1].append((a-1,c))
+
 k = int(input())
 pencils = [list(map(int, input().split())) for i in range(k)]
 pen = [-1 for i in range(n)]
 for i in pencils:
     pen[i[0]-1]=i[1]
+source = int(input())-1
 
-d = int(input())
+dist = [(0,source)]
+heapq.heapify(dist)
+dd = [float('inf') for j in range(n)]
+dd[source] = 0
+while dist:
+    u = heapq.heappop(dist)
+    d = u[0]
+    node = u[1]
+    for v in graph[node]:
+        alt = d+v[1]
+        if dd[v[0]]>alt:
+            dd[v[0]]=alt
+            heapq.heappush(dist, (alt, v[0]))
 
-graph = [[0 for j in range(n)] for i in range(n)]
-for i in roads:
-    graph[i[0]-1][i[1]-1]=i[2]
-    graph[i[1]-1][i[0]-1]=i[2]
-
-def dijkstra(graph, source):
-    dist = [float('inf') for i in range(len(graph))]
-    Q = [i for i in range(len(graph))]
-    dist[source] = 0
-    while Q:
-        mm = [float("inf"),0]
-        for i in Q:
-            if dist[i]<mm[0]:
-                mm = (dist[i], i)
-        u=mm[1]
-        Q.remove(u)
-        for v in Q:
-            if graph[u][v]!=0:
-                alt = dist[u]+graph[u][v]
-                if alt < dist[v]:
-                    dist[v]=alt
-    return dist
-out = dijkstra(graph, d-1)
-
-outout=[]
-for i in range(len(out)):
+outout=float('inf')
+for i in range(n):
     if pen[i]!=-1:
-        outout.append(out[i]+pen[i])
-print(min(outout))
+        outout = min(dd[i]+pen[i], outout)
+print(outout)
+#faster way??
