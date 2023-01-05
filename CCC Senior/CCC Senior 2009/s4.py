682579fde1eb76ab25f03102e403c3316d548cd2
kellen-sun <ksun5@ocdsb.ca>
2023-01-05 11:48:36 -0500

    s4 2009 FINALLY

--- CCC Senior/CCC Senior 2009/s4.py
+++ CCC Senior/CCC Senior 2009/s4.py
@@ -1,19 +1,15 @@
-#dijkstra's from the node u want a pencil from and find the cheapest choice.
+#then convert dijkstra's from the node u want a pencil from and find the cheapest choice.
 #add the pencil costs to all the destination city costs (removing the ones that don't sell)
 import sys
-import heapq
 input = sys.stdin.readline
 INF = 10**12
-
 n = int(input())
 t = int(input())
-#adjacency matrix uses too much memory
-graph = [[] for i in range(n)]
-for i in range(t):
-    a,b,c=map(int, input().split())
-    graph[a-1].append((b-1,c))
-    graph[b-1].append((a-1,c))
-
+graph = [[0 for j in range(n)] for i in range(n)]
+for j in range(t):
+    A,B,C = map(int, input().split())
+    graph[A-1][B-1]=C
+    graph[B-1][A-1]=C
 k = int(input())
 pencils = [list(map(int, input().split())) for i in range(k)]
 pen = [-1 for i in range(n)]
@@ -21,27 +17,24 @@ for i in pencils:
     pen[i[0]-1]=i[1]
 source = int(input())-1
 
-dd = [INF for j in range(n)]
-dd[source] = 0
-visited = set()
-while len(visited)!=n:
-    currentmin = INF
-    for i in range(len(dd)):
-        if dd[i]<currentmin and (i not in visited):
-            currentmin=dd[i]
-            currentminindex = i
-    d = currentmin
-    node = currentminindex
-    if node in visited:
-        continue
-    visited.add(node)
-    for v in graph[node]:
-        alt = d+v[1]
-        if dd[v[0]]>alt:
-            dd[v[0]]=alt
+dist = [float('inf') for i in range(len(graph))]
+Q = [i for i in range(len(graph))]
+dist[source] = 0
+while Q:
+    mm = [float("inf"),0]
+    for i in Q:
+        if dist[i]<mm[0]:
+            mm = (dist[i], i)
+    u=mm[1]
+    Q.remove(u)
+    for v in Q:
+        if graph[u][v]!=0:
+            alt = dist[u]+graph[u][v]
+            if alt < dist[v]:
+                dist[v]=alt
 
 outout=INF
 for i in range(n):
     if pen[i]!=-1:
-        outout = min(dd[i]+pen[i], outout)
+        outout = min(dist[i]+pen[i], outout)
 print(outout)
