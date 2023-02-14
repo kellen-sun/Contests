4b8a247da75b2ab2b4ece3b62c9f86d686315a41
kellen-sun <ksun5@ocdsb.ca>
2023-02-13 21:16:43 -0500

    2017 s2

--- CCC Senior/CCC Senior 2017/s2.py
+++ CCC Senior/CCC Senior 2017/s2.py
@@ -1,10 +1,12 @@
 n=int(input())
 tides=list(map(int,input().split()))
 t=sorted(tides)
-first=t[:n//2]
-second=t[n//2:]
+first=t[:(n+1)//2]
+second=t[(n+1)//2:]
 ans=[]
-for x in range(n//2):
-    ans.append(first[n//2-1-x])
-    ans.append(second[x])
+for x in range(n):
+    if x%2==0:
+        ans.append(first.pop(-1))
+    else:
+        ans.append(second.pop(0))
 print(*ans)
