0b919e83132801d180ce36697ba1651a781d5b83
kellen-sun <ksun5@ocdsb.ca>
2024-02-21 09:33:24 -0500

    22s4

--- CCC Senior/CCC Senior 2022/s4.py
+++ CCC Senior/CCC Senior 2022/s4.py
@@ -1,20 +1,27 @@
 n,c=map(int, input().split())
-points=list(map(int, input().split()))
+p=list(map(int, input().split()))
+points = [0 for i in range(c)]
+for i in p:
+    points[i] += 1
+#print(points)
 sol=0
-
-def works(triangle, c):
-    triangle=sorted(triangle)
-    if (triangle[1]-triangle[0])%c<c/2 and (triangle[2]-triangle[1])%c<c/2 and (triangle[0]-triangle[2])%c<c/2 and triangle[0]!=triangle[1] and triangle[2]!=triangle[1] and triangle[0]!=triangle[2]:
-        return True
-
-working=[]
-for i in range(n):
-    for j in range(n):
-        for k in range(n):
-            triangle=(points[i], points[j],points[k])
-            
-            if works(triangle, c):
-                working.append(str(sorted([i+1,j+1,k+1])))
-                sol+=1
-print(sol//6)
-#print(list(set(working)))
+circle = points + points
+pre = []
+s = sum(circle[:c//2+1])
+for i in range(c):
+    pre.append(s)
+    s -= circle[i]
+    s += circle[i+c//2+1]
+#print(pre)
+x = 0
+for i in range(c):
+    x += (pre[i]-points[i])*(pre[i]-points[i]-1)*points[i]//2 + (pre[i]-points[i])*points[i]*(points[i]-1)//2 + points[i]*(points[i]-1)*(points[i]-2)//6
+sol = n*(n-1)*(n-2)//6 - x
+if c%2 == 0:
+    toaddback = 0
+    for i in range(c):
+        A = circle[i]
+        B = circle[c//2+i]
+        toaddback += A*(A-1)*B + A*B*(B-1)
+    sol += toaddback//4
+print(sol)
